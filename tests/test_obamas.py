86e5ee09b56ab1044d2173a0149710f0c7be7d47
Alan Lee <alanhimself@outlook.com>
2025-04-06 00:06:19 -0700

    add completed tests

--- tests/test_obamas.py
+++ tests/test_obamas.py
@@ -1,1 +1,7 @@
 from obamas import obamas
+def test_is_obama_true():
+    assert obamas.is_obama(2008) is True
+    assert obamas.is_obama(2012) is True
+
+def test_is_obama_false():
+    assert obamas.is_obama(2016) is False
