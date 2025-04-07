1bc1fa46b802398ac2a479808809d6e0eb848b1e
Alan Lee <alanhimself@outlook.com>
2025-04-06 17:32:30 -0700

    adding example

--- src/obamas/obamas.py
+++ src/obamas/obamas.py
@@ -1,5 +1,20 @@
 def is_obama(num):
-    """Takes a number and checks if it is a year Obama won."""
+    """Takes a number and checks if it is a year Obama won.
+    
+        Parameters
+    ----------
+    is_obama :  int
+        A year as a whole number
+
+    Returns
+    -------
+    True or False
+
+    Examples
+    --------
+    >>> from obamas import obamas
+    >>> is_obama(2008)
+    """
 
     try:
         return (num == 2008 or num == 2012)
